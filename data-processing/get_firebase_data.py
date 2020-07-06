a943a7ba503bd64f9fda9f6e3276db8508058c59
KaviMD <kaviasher@gmail.com>
2020-07-06 11:15:01 -0700

    Add documentation to firebase scraper

--- data-processing/get_firebase_data.py
+++ data-processing/get_firebase_data.py
@@ -1,3 +1,8 @@
+# TODO: Give this script admin privileges
+# https://console.firebase.google.com/u/0/project/accessible-teleop/settings/serviceaccounts/adminsdk
+# https://firebase.google.com/docs/database/admin/start#python
+# https://firebase.google.com/docs/admin/setup#prerequisites
+
 # This script only works on python 3.6 and below, the firebase library has not been updated to work with any later version
 from firebase import firebase
 import simplejson as json
