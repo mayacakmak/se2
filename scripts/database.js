786324dbb92876ff26323e9b662276818016e092
KaviMD <kaviasher@gmail.com>
2020-07-17 16:30:56 -0700

    Add o-auth authentication

--- scripts/database.js
+++ scripts/database.js
@@ -2,7 +2,7 @@
 * Database class for interfacing with the Firebase realtime database
 */
 function Database() {
-  
+
   Database.isAnonymous = false;
   Database.uid = null; // User ID
   Database.sid = null; // Session ID
@@ -21,24 +21,57 @@ function Database() {
   * Firebase configuration information obntained from the Firebase console
   */
   Database.config = {
-      apiKey: "AIzaSyBLQXA1Lwlp6SgfQ3HAos_kCL4flCNc004",
-      authDomain: "accessible-teleop.firebaseapp.com",
-      databaseURL: "https://accessible-teleop.firebaseio.com",
-      projectId: "accessible-teleop",
-      storageBucket: "accessible-teleop.appspot.com",
-      messagingSenderId: "430487423155",
-      appId: "1:430487423155:web:edbef6c7dbbd04da"
-    };
+    apiKey: "AIzaSyBLQXA1Lwlp6SgfQ3HAos_kCL4flCNc004",
+    authDomain: "accessible-teleop.firebaseapp.com",
+    databaseURL: "https://accessible-teleop.firebaseio.com",
+    projectId: "accessible-teleop",
+    storageBucket: "accessible-teleop.appspot.com",
+    messagingSenderId: "430487423155",
+    appId: "1:430487423155:web:edbef6c7dbbd04da"
+  };
 
   this.database = Database.config["databaseURL"];
   this.app = firebase.initializeApp(Database.config);
 
-  this.initialize = function() {
+  this.initialize = function (anonymous = true) {
     firebase.auth().onAuthStateChanged(Database.handleAuthStateChange);
-    this.signInAnonymously();
+    if (anonymous) {
+      this.signInAnonymously();
+    } else {
+      this.signInWithGoogle();
+    }
   }
 
-  this.signInAnonymously = function() {
+  this.signInWithGoogle = function () {
+    console.log('sign in with google');
+    if (Database.uid == null) {
+      console.log('uid null')
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().useDeviceLanguage();
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(function (result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+          console.log(error);
+        });
+    }
+  }
+
+  this.signInAnonymously = function () {
     if (Database.uid == null) {
       firebase
         .auth()
@@ -47,13 +80,13 @@ function Database() {
     }
   }
 
-  Database.handleError = function(error) {
+  Database.handleError = function (error) {
     var errorCode = error.code;
     var errorMessage = error.message;
     console.log("Error " + errorCode + ": " + errorMessage);
   }
 
-  Database.handleAuthStateChange = function(user) {
+  Database.handleAuthStateChange = function (user) {
     if (user) {
       Database.isAnonymous = user.isAnonymous;
       Database.uid = user.uid;
@@ -61,7 +94,7 @@ function Database() {
       if (!Database.isAnonymous) {
         console.log("Signed in as " + user.displayName);
       } else {
-        console.log("Signed in anonymously");     
+        console.log("Signed in anonymously");
       }
 
       // Create directory in database to save this user's data
@@ -75,12 +108,12 @@ function Database() {
     }
   }
 
-  Database.signOut = function() {
+  Database.signOut = function () {
     firebase.auth().signOut().catch(Database.handleError);
     Database.uid = null;
   }
 
-  Database.insertTime = function(event) {
+  Database.insertTime = function (event) {
     var date = new Date();
     event.timestamp = date.getTime();
     event.date = date.toDateString();
@@ -88,7 +121,7 @@ function Database() {
     return event;
   }
 
-  Database.logSessionStart = function() {
+  Database.logSessionStart = function () {
     if (Database.isLogging) {
       var dir = 'users/' + Database.uid + '/sessions';
       var dbRef = firebase.database().ref(dir);
@@ -99,7 +132,7 @@ function Database() {
     }
   }
 
-  Database.logCycleStart = function(control, transitionType, targetPose) {
+  Database.logCycleStart = function (control, transitionType, targetPose) {
     if (Database.isLogging) {
       var dir = 'users/' + Database.uid + "/sessions/" + Database.sid + "/cycles/";
       var dbRef = firebase.database().ref(dir);
@@ -120,7 +153,7 @@ function Database() {
     }
   }
 
-  Database.logCycleFinish = function() {
+  Database.logCycleFinish = function () {
     if (Database.isLogging) {
       var dir = 'users/' + Database.uid + "/sessions/" + Database.sid + "/cycles/" + Database.cid;
       var dbRef = firebase.database().ref(dir);
@@ -134,20 +167,20 @@ function Database() {
       Database.cid = null;
     }
   }
-  
-  Database.logEvent = function(eventLog) {
+
+  Database.logEvent = function (eventLog) {
     if (Database.isLogging) {
       var dir = 'users/' + Database.uid + "/sessions/" + Database.sid + "/cycles/" + Database.cid + "/events";
       var dbRef = firebase.database().ref(dir);
       eventLog.type = "action"
-      eventLog.eePose = {x: ee.pose.x, y: ee.pose.y, theta: ee.pose.theta};
+      eventLog.eePose = { x: ee.pose.x, y: ee.pose.y, theta: ee.pose.theta };
       Database.insertTime(eventLog);
       dbRef.push(eventLog);
       console.log(eventLog);
     }
   }
 
-  Database.logEEPose = function() {
+  Database.logEEPose = function () {
     if (Database.isLogging) {
       var dir = 'users/' + Database.uid + "/sessions/" + Database.sid + "/cycles/" + Database.cid + "/events";
       var dbRef = firebase.database().ref(dir);
@@ -163,6 +196,10 @@ function Database() {
       dbRef.push(eventLog);
       console.log(eventLog);
     }
+  }
+
+  Database.getData = function () {
+    
   }
 }
 
