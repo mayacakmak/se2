baa2d716484d99d439c260ac6961daef39a5fa28
mayacakmak <mcakmak@cs.washington.edu>
2020-08-06 13:19:31 -0700

    making the countdown timer an option with a flag, since we might not want it during practice

--- scripts/workspace.js
+++ scripts/workspace.js
@@ -30,6 +30,7 @@ var control = null;
 
 // Enable/Disable logging
 var offline = true;
+var hasTimer = false;
 
 function loadInterface() {
   let controlParam = getURLParameter("c");
@@ -49,39 +50,44 @@ function loadInterface() {
 }
 
 function initializeTest() {
-  // Set the date we're counting down to
-  var countDownDate = new Date().getTime()+ 5000;
+	if (hasTimer) {
+	  // Set the date we're counting down to
+	  var countDownDate = new Date().getTime()+ 5000;
 
-  var timer = new Timer();
-  timer.setVisible(true);
-  timer.setText("");
+	  var timer = new Timer();
+	  timer.setVisible(true);
+	  timer.setText("");
 
-  console.log("text visible")
+	  console.log("text visible")
 
-  // Update the count down every 1 second
-  var x = setInterval(function () {
+	  // Update the count down every 1 second
+	  var x = setInterval(function () {
 
-    // Get today's date and time
-    var now = new Date().getTime();
+	    // Get today's date and time
+	    var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+	    // Find the distance between now and the count down date
+	    var distance = countDownDate - now;
 
-    // Time calculations for seconds
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	    // Time calculations for seconds
+	    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    timer.setText(seconds);
+	    timer.setText(seconds);
 
-    // If the count down is finished, write some text
-    if (seconds < 1) {
-      clearInterval(x);
-      timer.setText("Go");
-      setTimeout(function () {
-        timer.setVisible(false);
-        setupEnvironment();
-      }, 1000)
-    }
-  }, 1000);
+	    // If the count down is finished, write some text
+	    if (seconds < 1) {
+	      clearInterval(x);
+	      timer.setText("Go");
+	      setTimeout(function () {
+	        timer.setVisible(false);
+	        setupEnvironment();
+	      }, 1000)
+	    }
+	  }, 1000);
+	}
+	else {
+		setupEnvironment();
+	}
 }
 
 /*
