198ede79aacaca0c3c3e650e149e8c57ba28b6ae
KaviMD <kaviasher@gmail.com>
2020-09-10 21:36:32 -0700

    Add uid and sid to questionnaire.csv

--- data-processing/json_to_csv.py
+++ data-processing/json_to_csv.py
@@ -20,7 +20,7 @@ for interface_num in json_snapshot['state']:
 cycle_data_columns = ["startTime", "endTime", "control", "transitionType", "targetX", "targetY", "targetTheta", "threshXY", "threshTheta", "uid", "sid", "cid"]
 cycle_data = []
 
-questionnaire_data_columns = ['section-0-question-0','section-0-question-1', 'section-0-question-10', 'section-0-question-11', 'section-0-question-12', 'section-0-question-13', 'section-0-question-2', 'section-0-question-3', 'section-0-question-4', 'section-0-question-5', 'section-0-question-6', 'section-0-question-7', 'section-0-question-8', 'section-0-question-9', 'section-1-question-0', 'section-1-question-1', 'section-1-question-10', 'section-1-question-11', 'section-1-question-12', 'section-1-question-13', 'section-1-question-14', 'section-1-question-2', 'section-1-question-3', 'section-1-question-4', 'section-1-question-5', 'section-1-question-6', 'section-1-question-7', 'section-1-question-8', 'section-1-question-9', 'section-2-question-0', 'section-2-question-1', 'section-2-question-2', 'section-2-question-3', 'section-2-question-4', 'section-2-question-5', 'section-2-question-6', 'section-2-question-7', 'section-2-question-8']
+questionnaire_data_columns = ['uid-1000--00','sid-1000--00','section-0-question-0','section-0-question-1', 'section-0-question-10', 'section-0-question-11', 'section-0-question-12', 'section-0-question-13', 'section-0-question-2', 'section-0-question-3', 'section-0-question-4', 'section-0-question-5', 'section-0-question-6', 'section-0-question-7', 'section-0-question-8', 'section-0-question-9', 'section-1-question-0', 'section-1-question-1', 'section-1-question-10', 'section-1-question-11', 'section-1-question-12', 'section-1-question-13', 'section-1-question-14', 'section-1-question-2', 'section-1-question-3', 'section-1-question-4', 'section-1-question-5', 'section-1-question-6', 'section-1-question-7', 'section-1-question-8', 'section-1-question-9', 'section-2-question-0', 'section-2-question-1', 'section-2-question-2', 'section-2-question-3', 'section-2-question-4', 'section-2-question-5', 'section-2-question-6', 'section-2-question-7', 'section-2-question-8']
 questionnaire_data = []
 
 for uid in json_snapshot['users']:
@@ -58,7 +58,7 @@ for uid in json_snapshot['users']:
 
             if 'questionnaires' in json_snapshot['users'][uid]['sessions'][sid]:
                 for qid in json_snapshot['users'][uid]['sessions'][sid]['questionnaires']:
-                    answers = []
+                    answers = [uid, sid]
                     for question_name in json_snapshot['users'][uid]['sessions'][sid]['questionnaires'][qid]['answers']:
                         answers.append(json_snapshot['users'][uid]['sessions'][sid]['questionnaires'][qid]['answers'][question_name])
                     questionnaire_data.append(answers)
@@ -74,10 +74,14 @@ cycles_df['targetDistance'] = ((cycles_df['targetX'] - 357) ** 2 + (cycles_df['t
 # Save the cycles to disk
 cycles_df.to_csv(snapshot_name+"-cycles.csv")
 
+
 questionnaires_df = pd.DataFrame(questionnaire_data, columns=questionnaire_data_columns)
 
 # Sort the dataframe columns by question number. The sorting code is complex, because we are sorting by both section and question number
 questionnaires_df = questionnaires_df.reindex(sorted(questionnaires_df.columns, key=lambda x: int("".join([x.split('-')[index] for index in [1,3]]))), axis=1)
+
+# Rename the uid and sid columns
+questionnaires_df.rename(columns={"uid-1000--00": "uid", "sid-1000--00": "sid"}, inplace=True)
 
 # Save the questionnaires to disk
 questionnaires_df.to_csv(snapshot_name+"-questionnaires.csv")
