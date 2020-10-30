7f67728ebbc4fceaf40bc0148f8e15ae19b72181
tapomayukh <tapomayukh@gmail.com>
2020-10-29 20:00:50 -0700

    Changes to be committed: 	modified:   process_data.py 	modified:   process_data_paper.py

--- data-processing/process_data_paper.py
+++ data-processing/process_data_paper.py
@@ -19,6 +19,8 @@ import simplejson as json
 import time
 import copy
 
+from process_data import interface_dfs
+
 # %%
 # Custom tools
 
@@ -60,119 +62,6 @@ def resample(x, n, kind='linear'):
     return f(np.linspace(0, 1, n))
 
     
-
-# %% [markdown]
-## Firebase snapshot and other inputs
-
-# %%
-snapshot_folder = "firebase-snapshots"
-snapshot_name = "accessible-teleop-export-10-29-study"
-
-# %% [markdown]
-## Load data from Firebase
-# The data is loaded from a .json file and converted into a pandas DataFrame, and a dictionary.
-
-# For most of the processing we do, only the pandas dataframe [cycles_df] is necessary.
-# It contains cycle-level data about:
-# <br>
-# - The start and end of a cycle
-# - The control scheme that was used
-# - The status of the cycle (incomplete or complete)
-# - The pose (X, Y, and Theta) of the target
-#
-#The dictionary [action_list] is organized by interfaceID, and contains all the action chains that made up cycles
-#It is used to calcuate specific data about specific actions (for example % of time spent orienting vs translating)
-#
-# %%
-json_snapshot = {}
-with open(os.path.join(snapshot_folder, snapshot_name + ".json")) as f:
-    json_snapshot = json.load(f)
-user_data = json_snapshot['users'] 
-state_data = json_snapshot['state'] 
-
-# Gather UIDs from states/`interface_num`/completed
-uids = []
-for interface_num in state_data:
-    uids += interface_num['complete'].keys()
-
-# %%
-# [interfaceIDs] is a set that contains one of each ID
-# We use it later on to separate the dataframe by interface
-interfaceIDs = set()
-
-cycle_data_columns = ["startTime", "endTime", "status", "control", "transitionType", "interfaceID", "targetX", "targetY", "targetTheta", "threshXY", "threshTheta"]
-cycle_data = []
-
-action_list = {}
-
-for uid in user_data:
-    if uid in uids:
-        for sid in user_data[uid]['sessions']:
-            if 'cycles' in user_data[uid]['sessions'][sid]:
-                for cid in user_data[uid]['sessions'][sid]['cycles']:
-                    cycle = user_data[uid]['sessions'][sid]['cycles'][cid]
-                    
-                    if 'isTest' not in cycle:
-                        continue
-
-                    if cycle['isTest']:
-                        # Update cycle_data with general information about the cycle
-                        startTime = cycle['startTime']['timestamp'] / 1000
-                        status = cycle['status']
-
-                        # There is no end timestamp if the cycle is incomplete
-                        endTime = startTime
-                        if status == "complete":
-                            endTime = cycle['endTime']['timestamp'] / 1000
-
-                        control = cycle['control']
-                        transitionType = cycle['transitionType']
-                        interfaceID = control + "." +transitionType
-
-                        interfaceIDs.add(interfaceID)
-
-                        targetX = cycle['targetPose']['x']
-                        targetY = cycle['targetPose']['y']
-                        targetTheta = cycle['targetPose']['theta']
-                        threshXY = cycle['targetPose']['threshXY']
-                        threshTheta = cycle['targetPose']['threshTheta']
-
-                        cycle_data.append([startTime, endTime, status, control, transitionType, interfaceID, targetX, targetY, targetTheta, threshXY, threshTheta])
-
-                        # Update action_list with the set of actions for this cycle
-                        if 'events' in cycle: # Sometimes the last cycle of a session has no action so we skip it
-                            actions = []
-                            for aid in cycle['events']:
-
-                                # We want to ignore any events that are just ee pose logs, and only keep user actions
-                                if cycle['events'][aid]['type'] != 'pose':
-                                    # We want to remove any actions that are just a release of the cursor
-                                    action_type = cycle['events'][aid]['newState']
-                                    if action_type != "cursor-free":
-                                        actions.append(action_type)
-                                    elif interfaceID == "target.click" or interfaceID == "targetdrag.click":
-                                        actions.append(action_type)
-                            
-                            if interfaceID in action_list:
-                                action_list[interfaceID].append(actions)
-                            else:
-                                action_list[interfaceID] = [actions]
-
-# %%
-cycles_df = pd.DataFrame(cycle_data, columns=cycle_data_columns)
-cycles_df['cycleLength'] = cycles_df['endTime'] - cycles_df['startTime']
-
-# Calculate the euclidean distace between where the ee starts (357, 249) and the target
-cycles_df['targetDistance'] = ((cycles_df['targetX'] - 357) ** 2 + (cycles_df['targetY'] - 249) ** 2) ** 0.5
-cycles_df = cycles_df[cycles_df["status"] == "complete"]
-
-cycles_df['targetPosTheta'] = np.degrees(np.arccos((cycles_df['targetX'] - 357)/cycles_df['targetDistance']))
-
-interface_dfs = {}
-for interfaceID in interfaceIDs:
-    interface_dfs[interfaceID] = cycles_df[cycles_df["interfaceID"] == interfaceID]
-
-cycles_df.head()
 
 # %% [markdown]
 ## Time stats per interface
