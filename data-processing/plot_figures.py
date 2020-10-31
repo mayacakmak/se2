0422f5fd5cf0872214913d1bc8270912b18cde30
MAYA CAKMAK <mayacakmak@MAYAs-MacBook-Air.local>
2020-10-31 02:09:34 -0700

    major refactoring of plotting code

--- data-processing/plot_figures.py
+++ data-processing/plot_figures.py
@@ -5,6 +5,10 @@
 # Data Processing
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["font.size"] = 12
+plt.rcParams["axes.labelsize"] = 'x-large'
+
 from matplotlib.collections import LineCollection
 import scipy as scp
 from scipy import interpolate
@@ -21,9 +25,22 @@ import copy
 
 # from process_data import interface_dfs
 
+
+# targetbarclicks = ['panel.click','arrow.click','drag.click','targetdrag.click','target.click']
+# targetbarprs = ['panel.press/release','arrow.press/release','drag.press/release','targetdrag.press/release', 'targetdrag.press/release']
+targetbarclicks = ['panel-click','arrow-click','drag-click','targetdrag-click','target-click']
+targetbarprs = ['panel-press/release','arrow-press/release','drag-press/release','targetdrag-press/release', 'targetdrag-press/release']
+
+#targetbarnames = ['arrow.click','drag.click','panel.click','target.click','targetdrag.click','arrow.p/r','drag.p/r','panel.p/r','targetdrag.p/r']
+targetplotnames = ['Fixed','ArrowRing','CircleRing','TargetAnchor','TargetRing']
+targetplotcolors = ['#ffe500','#ff9405','#ff4791','#007bff','#00c36b']
+targetplotcolorslight = ['#ffe486','#ffb757','#ff8dbb','#64afff','#00fd8b']
+
+interfaceIDs = ['arrow-click','drag-click','panel-click','target-click','targetdrag-click','arrow-press/release','drag-press/release','panel-press/release','targetdrag-press/release']
+
+
 def plot_everything():
     # interfaceIDs = ['arrow.click','drag.click','panel.click','target.click','targetdrag.click','arrow.press/release','drag.press/release','panel.press/release','targetdrag.press/release']
-    interfaceIDs = ['arrow-click','drag-click','panel-click','target-click','targetdrag-click','arrow-press/release','drag-press/release','panel-press/release','targetdrag-press/release']
     cycles_df = pd.read_csv("data/se2-10-29-filtered-cycles.csv", skiprows = 0)
 
     print(cycles_df.columns)
@@ -43,6 +60,10 @@ def plot_everything():
     for interfaceID in interfaceIDs:
         interface_dfs[interfaceID] = user_dfs[user_dfs["interfaceID"] == interfaceID]
 
+    all_interface_dfs = {}
+    for interfaceID in interfaceIDs:
+        all_interface_dfs[interfaceID] = cycles_df[cycles_df["interfaceID"] == interfaceID]
+
     # %%
     for interfaceID in interface_dfs:
         interface_df = interface_dfs[interfaceID]
@@ -57,80 +78,14 @@ def plot_everything():
     # %% [markdown]
     ## Time stats per interface
 
-    # %%
-    fig = plt.figure(figsize=(16,8))
-    fig.subplots_adjust(hspace=0.6, wspace=0.3)
-    ax = fig.add_subplot(1,1,1)
+    # plot_bar_chart(interface_dfs, 'cycleLength', 'Task completion time (sec)')
+    # plot_bar_chart(interface_dfs, 'numClicks', 'Number of clicks')
+    # plot_bar_chart(interface_dfs, 'draggingDuration', 'Drag duration (sec)')
 
-    # targetbarclicks = ['panel.click','arrow.click','drag.click','targetdrag.click','target.click']
-    # targetbarprs = ['panel.press/release','arrow.press/release','drag.press/release','targetdrag.press/release', 'targetdrag.press/release']
-    targetbarclicks = ['panel-click','arrow-click','drag-click','targetdrag-click','target-click']
-    targetbarprs = ['panel-press/release','arrow-press/release','drag-press/release','targetdrag-press/release', 'targetdrag-press/release']
-
-    #targetbarnames = ['arrow.click','drag.click','panel.click','target.click','targetdrag.click','arrow.p/r','drag.p/r','panel.p/r','targetdrag.p/r']
-    targetplotnames = ['Fixed','ArrowRing','CircleRing','TargetAnchor','TargetRing']
-    targetplotcolors = ['#ffe500','#ff9405','#ff4791','#007bff','#00c36b']
-
-    interfaces = []
-    means_clicks = []
-    means_prs = []
-
-    errors_prs = []
-    errors_clicks = []
-
-    for i in np.arange(len(targetbarclicks)):
-        interface_dfclicks = interface_dfs[targetbarclicks[i]]
-        interface_dfprs = interface_dfs[targetbarprs[i]]
-        
-        means_clicks.append(np.mean(interface_dfclicks['cycleLength']))
-        means_prs.append(np.mean(interface_dfprs['cycleLength']))
-        
-        errors_clicks.append(np.std(interface_dfclicks['cycleLength']))
-        errors_prs.append(np.std(interface_dfprs['cycleLength']))
-
-        #print("Mean:", np.mean(interface_dfclicks['cycleLength']))
-        #print("Standard Deviation:",np.std(interface_dfclicks['cycleLength']))
-        #print("Min:",np.min(interface_dfclicks['cycleLength']))
-        #print("Max:",np.max(interface_dfclicks['cycleLength']))
-        #print()
-
-    means_prs[4] = 0
-    errors_prs[4] = 0
-
-    ax.grid(color='gray', linestyle='-.', linewidth=1, axis='x', which='major', zorder=0)
-
-    y_pos = np.arange(len(targetplotnames))
-    width = 0.35
-    rects1 = ax.barh(y_pos - width/2, means_prs, width, xerr=errors_prs, alpha=1.0, color="gray", ecolor="gray", capsize=16, label="P/R", zorder=2)
-    rects2 = ax.barh(y_pos + width/2, means_clicks, width, xerr=errors_clicks, alpha=1.0, color='black', ecolor="black", capsize=16, label="Click", zorder=2)
-    ax.set_ylabel('Interface',fontsize=16)
-    ax.set_xlabel('Time (s)',fontsize=16)
-    ax.set_yticks(y_pos)
-    ax.set_yticklabels(targetplotnames)
-    #ax.set_title('Task Completion Time', fontsize=24, fontweight='bold')
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['bottom'].set_linewidth(3.0)
-    ax.spines['left'].set_linewidth(3.0)
-    ax.yaxis.set_ticks_position('left')
-    ax.xaxis.set_ticks_position('bottom')
-    ax.tick_params(axis='both', which='major', labelsize=16)
-    ax.set_xlim([0, 40])
-    #plt.xticks(rotation=45)
-    ax.invert_yaxis()
-
-    for ytick, color in zip(ax.get_yticklabels(), targetplotcolors):
-        ytick.set_color(color)
-
-    for rect1 in rects1[0:4]:
-        ax.text(0.1, rect1.get_y() + 0.22, '$\it{P/R}$', c="white", fontsize=12, fontweight='bold')
-
-    for rect2 in rects2:
-        ax.text(0.1, rect2.get_y() + 0.22, '$\it{Click}$', c="white", fontsize=12, fontweight='bold')
-
-    plt.tight_layout()
+    plot_scatter(all_interface_dfs)
 
 
+def plot_scatter(interface_dfs):
     # %% [markdown]
     ## Time vs. Distance (Euclidean, Orientation, and Combined)
     ### Euclidean Distance vs Time
@@ -139,25 +94,24 @@ def plot_everything():
     fig = plt.figure(figsize=(16,8))
     fig.subplots_adjust(hspace=0.6, wspace=0.3)
     targetplots = ['panel-click','arrow-click','drag-click','targetdrag-click','target-click']
-    targetplotnames = ['Fixed','ArrowRing','CircleRing','TargetAnchor','TargetRing']
-    targetplotcolors = ['#ffe500','#ff9405','#ff4791','#007bff','#00c36b']
     interface_dftargets = [interface_dfs[idx] for idx in targetplots]
+    
     for i, interface_df in enumerate(interface_dftargets):
         ax = fig.add_subplot(2,5,str(i+1))
         bx = fig.add_subplot(2,5,str(i+6))
-        ax.set_title(targetplotnames[i], c=targetplotcolors[i], fontsize=16)
+        ax.set_title(targetplotnames[i], fontsize=24)
         #bx.set_title(targetplotnames[i], c=targetplotcolors[i], fontsize=16)
 
-        ax.scatter(interface_df['targetDistance'], interface_df['cycleLength'], c="tab:blue")
-        bx.scatter(interface_df['threshXY'], interface_df['cycleLength'], c="tab:orange")
+        ax.scatter(interface_df['targetDistance'], interface_df['cycleLength'], c=targetplotcolors[i], marker=".")
+        bx.scatter(interface_df['threshXY'], interface_df['cycleLength'], c=targetplotcolors[i], marker=".")
         lineax = fit_line(interface_df['targetDistance'], interface_df['cycleLength'])
         linebx = fit_line(interface_df['threshXY'], interface_df['cycleLength'])
         r_squaredax = lineax[2]
         r_squaredbx = linebx[2]
-        ax.plot(lineax[0], lineax[1], c="tab:red", linewidth=3.0)
-        bx.plot(linebx[0], linebx[1], c="tab:green", linewidth=3.0)
-        ax.text(80, 1, '$\mathbf{R^2}$ = %0.2f' %(1-r_squaredax), c="tab:red", fontsize=12, fontweight='bold')
-        bx.text(10, 1, '$\mathbf{R^2}$ = %0.2f' %(1-r_squaredbx), c="tab:green", fontsize=12, fontweight='bold')
+        ax.plot(lineax[0], lineax[1], c="black", linewidth=2.0)
+        bx.plot(linebx[0], linebx[1], c="black", linewidth=2.0)
+        ax.text(80, 30, '$\mathbf{R^2}$ = %0.2f' %(1-r_squaredax), c="black", fontsize=20)
+        bx.text(10, 30, '$\mathbf{R^2}$ = %0.2f' %(1-r_squaredbx), c="black", fontsize=20)
         
         #_, _, r_val, _, _ = scp.stats.linregress(interface_df['targetDistance'], interface_df['cycleLength'])
         #print(r_val**2)
@@ -178,19 +132,97 @@ def plot_everything():
         bx.xaxis.set_ticks_position('bottom')
 
         # Change the fontsize of tick labels
-        ax.tick_params(axis='both', which='major', labelsize=16)
-        bx.tick_params(axis='both', which='major', labelsize=16)
+        ax.tick_params(axis='both', which='major', labelsize=20)
+        bx.tick_params(axis='both', which='major', labelsize=20)
 
-        ax.set_xlabel('Distance to Target', fontsize=16)
-        ax.set_ylim([0, 50])
+        ax.set_xlabel('Target distance', fontsize=24)
+        ax.set_ylim([0, 40])
         
-        bx.set_xlabel('X-Y threshold', fontsize=16)
-        bx.set_ylim([0, 50])
+        bx.set_xlabel('Target size', fontsize=24)
+        bx.set_ylim([0, 40])
         
         if i == 0:
-            ax.set_ylabel('Cycle Time', fontsize=16)
-            bx.set_ylabel('Cycle Time', fontsize=16)
-    plt.show()
+            ax.set_ylabel('Time', fontsize=24)
+            bx.set_ylabel('Time', fontsize=24)
+    
+
+    plt.tight_layout()
+    plt.savefig('data/scatter.pdf')
+    # plt.show()
+
+
+
+
+def plot_bar_chart(interface_dfs, label, y_label):
+    # %%
+    fig = plt.figure(figsize=(10,8))
+    fig.subplots_adjust(hspace=0.6, wspace=0.3)
+    ax = fig.add_subplot(1,1,1)
+
+    means_clicks = []
+    means_prs = []
+
+    errors_prs = []
+    errors_clicks = []
+
+    for i in np.arange(len(targetbarclicks)):
+        interface_dfclicks = interface_dfs[targetbarclicks[i]]
+        interface_dfprs = interface_dfs[targetbarprs[i]]
+        
+        means_clicks.append(np.mean(interface_dfclicks[label]))
+        means_prs.append(np.mean(interface_dfprs[label]))
+        
+        errors_clicks.append(np.std(interface_dfclicks[label]))
+        errors_prs.append(np.std(interface_dfprs[label]))
+
+        #print("Mean:", np.mean(interface_dfclicks['cycleLength']))
+        #print("Standard Deviation:",np.std(interface_dfclicks['cycleLength']))
+        #print("Min:",np.min(interface_dfclicks['cycleLength']))
+        #print("Max:",np.max(interface_dfclicks['cycleLength']))
+        #print()
+
+    means_prs[4] = 0
+    errors_prs[4] = 0
+
+    ax.grid(color='gray', linestyle='-.', linewidth=1, axis='x', which='major', zorder=0)
+
+    y_pos = np.arange(len(targetplotnames))
+    width = 0.45
+    rects1 = ax.barh(y_pos - width/2, means_prs, width, xerr=errors_prs, 
+        alpha=1.0, color=targetplotcolorslight, ecolor="gray", capsize=16, zorder=2)
+    rects2 = ax.barh(y_pos + width/2, means_clicks, width, xerr=errors_clicks, 
+        alpha=1.0, color=targetplotcolors, ecolor="gray", capsize=16, zorder=2)
+    # ax.set_ylabel('Interface',fontsize=24)
+    ax.set_xlabel(y_label,fontsize=24)
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(targetplotnames)
+    #ax.set_title('Task Completion Time', fontsize=24, fontweight='bold')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_linewidth(2.0)
+    ax.spines['left'].set_linewidth(2.0)
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.tick_params(axis='both', which='major', labelsize=24)
+    ax.set_xlim([0, 25])
+    #plt.xticks(rotation=45)
+    ax.invert_yaxis()
+
+    # for ytick, color in zip(ax.get_yticklabels(), targetplotcolors):
+        # ytick.set_color(color)
+
+    for rect1 in rects1[0:4]:
+        ax.text(0.1, rect1.get_y() + 0.32, '$\it{P/R}$', c="white", fontsize=22, fontfamily="Times New Roman")
+
+    for rect2 in rects2:
+        ax.text(0.1, rect2.get_y() + 0.32, '$\it{Click}$', c="white", fontsize=22, fontfamily="Times New Roman")
+
+    plt.tight_layout()
+    plt.savefig('data/' + label + '.pdf')
+    # plt.show()
+
+
+
 
 
 # %%
